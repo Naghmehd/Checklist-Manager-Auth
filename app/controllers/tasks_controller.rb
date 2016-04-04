f0db8cefcc3538534f3009d38fea62be58aadd4b
Naghmeh Shirazi <naghmehd@gmail.com>
2016-04-04 16:26:41 -0500

    add the form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,44 @@
 class TasksController < ApplicationController
-  
+  def index
+    @task = Task.new
+    @tasks = Task.order(position: :asc)
+  end
+
+  def show
+    task = Task.find(params[:id])
+    if task
+      @task = task
+    end
+  end
+
+  def new
+    redirect_to root_path
+  end
+
+  def create
+    @task = Task.new
+    @task.body = params[:task][:body]
+    @task.position = params[:task][:position]
+    @task.completed = false
+
+    if @task.save
+      flash[:notice] = "Task added"
+      redirect_to root_path
+    else
+      flash[:alert] = "Task could not be saved"
+      @tasks = Task.order(position: :asc)
+      render :index
+    end
+  end
+
+  def  update
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+    flash[:alert] = "Task deleted"
+  end
+
 end
